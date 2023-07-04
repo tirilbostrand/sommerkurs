f7d899ba9d3feb8b610b0a37cbb14a3c24f5e202
tirilbostrand <138440237+tirilbostrand@users.noreply.github.com>
2023-07-04 14:43:09 +0200

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+name = input("name of a movie character")
+transport = input("something you would use for transport")
+place = input("name a show or movie(cant be the show your character is from")
+villan = input("name of a villan:n")
+colour = input("name a colour: ")
+adtv = input("give me an adjective: ")
+sub = input("now a subjective: ")
+animal = input("name of an animal: ")
+sidekick = input("another character(cant be the same as one youve already taken): ")
+adtv2 = input("a word for suprisingly: ")
+weapon = input("something you would give to a loved one: ")
+wepon = input("something you would beat someone with: ")
+holiday = input("a place you never want to visit: ")
+
+
+print(f"\n {name} was taking a walk, when suddenly a mysterius {transport} showed up right in front of them. they ofcourse walked into the {transport} and was taken to the universe of {place}. when they arrived there {villan} was waiting for them. in their hand was a {adtv} {colour} {sub}. then suddenly a flocki of {animal}s came storming through. {name} picked one of them up and trew it at {villan}. {villan} got hit right in the face,
+and just as {name} thought they had won, {villan} threw one of the {animal}s at {name}. {name} now almost defeated, only had one thing they could do. call on their trusty sidekick: {sidekick}. through the door {adtv2} came {sidekick}. with this i shall defeat you said {sidekick}. as he held fourth {weapon}. how are you supposed to defeat me with a {weapon} when i have this. said the villan holding fourth a {wepon}. an epic battle ensued, one that is still being told of today, but atlast {sidekick} won. and {name} and {sidekick} went to {holiday} to live their lives happily ever after.\n")
